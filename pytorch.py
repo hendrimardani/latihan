440c9cc9aa882f50f07b4a904346a3ee4e3a7f12
Hendri Mardani <hendrimardani290301@gmail.com>
2023-03-04 06:57:07 +0700

    COmmit

--- pytorch.py
+++ pytorch.py
@@ -8,15 +8,20 @@ class Hitung:
         self.per = Hitung.perhitungan
         
     def hasil(self):
+        """
+            input2 x alpha + input1 
+        """
         self.mt_a = torch.randint(self.a, self.b, (5,))
         self.mt_b = torch.randint(self.a, self.b, (5,))
         self.hasil = torch.add(self.mt_a, self.mt_b)
 
         return self.mt_a, self.mt_b, self.hasil
     
-    def hasil2(self):
-        a = self.hasil()[0]
-        return a
+    def __str__(self):
+        mt_a, mt_b, mt_hasil = self.hasil()
+        return f"Matrix A adalah mt_a {mt_a}\n\
+                 Matrix B adalah mt_b {mt_b}\n\
+                 Hasil Matirx adalah {mt_hasil}"
     
 hasil = Hitung(5, 10)
-print(hasil.hasil2())
+print(hasil)
