b95c4a5c2ebb014c9a4ac2d9bd6ee59d22c687c5
Hendri Mardani <hendrimardani290301@gmail.com>
2023-03-03 20:15:54 +0700

    commit

--- pytorch.py
+++ pytorch.py
@@ -1,17 +1,22 @@
 import torch
 
 class Hitung:
+    perhitungan = 12
     def __init__(self, a, b):
         self.a = a
         self.b = b
-
-    def hasil(self):
-        a = torch.randint(self.a, self.b, (5,))
-        b = torch.randint(self.a, self.b, (5,))
-        self.hasil = torch.add(a, b)
-
-        return self.hasil, a, b
+        self.per = Hitung.perhitungan
         
+    def hasil(self):
+        self.mt_a = torch.randint(self.a, self.b, (5,))
+        self.mt_b = torch.randint(self.a, self.b, (5,))
+        self.hasil = torch.add(self.mt_a, self.mt_b)
 
+        return self.mt_a, self.mt_b, self.hasil
+    
+    def hasil2(self):
+        a = self.hasil()[0]
+        return a
+    
 hasil = Hitung(5, 10)
-print(hasil.hasil())
+print(hasil.hasil2())
