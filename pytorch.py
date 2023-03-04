85dc2068bc637fe4a116bcac1fc325440f839a44
Hendri Mardani <hendrimardani290301@gmail.com>
2023-03-04 11:25:58 +0700

    aa

--- pytorch.py
+++ pytorch.py
@@ -18,22 +18,33 @@ class Hitung:
 
         return self.mt_a, self.mt_b, self.hasil
     
+    @property
     def meratakan(self):
-        self.flat1 = torch.tensor(self.flat1).view(1,-1,1)
-        self.flat2 = torch.tensor(self.flat2).view(1,-1,1)
+        flat1 = torch.tensor(self.flat1).view(1,-1,1)
+        flat2 = torch.tensor(self.flat2).view(1,-1,1)
 
-        self.hasil2 = self.flat1.squeeze()
-        return self.flat1, self.flat2, self.hasil2
+        hasil2 = flat1.squeeze()
+        return flat1, flat2, hasil2
 
     def __str__(self):
         mt_a, mt_b, mt_hasil = self.hasil()
-        flat1, flat2, hasil2 = self.meratakan()
+        flat1, flat2, hasil2 = self.meratakan
 
         # return f"Matrix A adalah mt_a {mt_a}\n\
         #          Matrix B adalah mt_b {mt_b}\n\
         #          Hasil Matirx adalah {mt_hasil}"
         return f"flat1 = {flat1} \n flat2 = {flat2} \n Hasil = {hasil2}"
     
-    
-hasil = Hitung(1, 2, flat1=[1,2, 3], flat2=[4, 5, 6])
+# class Hitung2(Hitung):
+#     def __init__(self, a=None):
+#         super(Hitung2).__init__()
+
+#         def hitung2(self):
+#             a = torch.tensor(self.a)
+
+#             return a
+
+#         def __str__(self):
+
+hasil = Hitung(1, 2, flat1=[[1,2,3], [4,5,6]], flat2=[4, 5, 6])
 print(hasil)
